cfa8f76b3c493e0ffac574defb93c82a7ff470e5
peter <petersp2000@gmail.com>
2023-05-30 19:50:58 +0100

    linting redis.js

--- utils/redis.js
+++ utils/redis.js
@@ -4,26 +4,26 @@ import { promisify } from 'util';
 class RedisClient {
   constructor() {
     this.client = redis.createClient();
-    this.connected = true;
-    this.client.on('error', (err) => {
-      console.log(`Redis client not connected to the server: ${err}`);
-      this.connected = false;
-    });
+    this.client.on('error', (err) => console.log(`Redis client not connected to the server: ${err}`));
   }
+
   isAlive() {
-    return this.connected;
+    return this.client.connected;
   }
+
   async get(key) {
     const getValue = await promisify(this.client.get).bind(this.client, key);
     const value = await getValue();
     return value;
   }
+
   async set(key, value, duration) {
     const setex = await promisify(this.client.setex).bind(this.client);
     await setex(key, duration, value);
   }
+
   async del(key) {
-    await promisify(this.client.del).bind(this.client, key)
+    await promisify(this.client.del).bind(this.client, key);
   }
 }
 const redisClient = new RedisClient();
