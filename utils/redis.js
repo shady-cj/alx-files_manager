3017c4eaf22f114dea32b9860e1042ccb9093352
peter <petersp2000@gmail.com>
2023-05-31 14:20:41 +0100

    setting up express server

--- utils/redis.js
+++ utils/redis.js
@@ -6,20 +6,25 @@ class RedisClient {
     this.client = redis.createClient();
     this.client.on('error', (err) => console.log(`Redis client not connected to the server: ${err}`));
   }
+
   isAlive() {
     return this.client.connected;
   }
+
   async get(key) {
     const getValue = await promisify(this.client.get).bind(this.client, key);
     const value = await getValue();
     return value;
   }
+
   async set(key, value, duration) {
     const setex = await promisify(this.client.setex).bind(this.client);
     await setex(key, duration, value);
   }
+
   async del(key) {
-    await promisify(this.client.del).bind(this.client, key)
+    const del = await promisify(this.client.del).bind(this.client);
+    await del(key);
   }
 }
 const redisClient = new RedisClient();
